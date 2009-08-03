b5a2ebcd1fd836e74b847fbffe3593b3ed8376ea
Harold <hhausman@gmail.com>
2009-08-02 21:30:28 -0600

    gluUnProject checkpoint.

--- clj-jogl.clj
+++ clj-jogl.clj
@@ -4,7 +4,8 @@
                            MouseMotionListener MouseWheelListener]
            [com.sun.opengl.util FPSAnimator]
            [javax.media.opengl GLCanvas GLEventListener GL]
-           [javax.media.opengl.glu GLU]))
+           [javax.media.opengl.glu GLU]
+           [java.io File FileWriter]))
 
 (defn get-window-adapter [frame animator]
   (proxy [WindowAdapter] []
@@ -12,8 +13,90 @@
                          (.stop animator)
                          (.setVisible frame false)))))
 
+(.delete (File. "C:\\log.log"))
+(defn log [& strings]
+  (doto (FileWriter. "C:\\log.log" true)
+    (.write (apply str strings))
+    (.write "\r\n")
+    (.close))
+  nil)
+
+(defn log-matrix [m]
+  (log (nth m  0) " " (nth m  1) " " (nth m  2) " " (nth m  3))
+  (log (nth m  4) " " (nth m  5) " " (nth m  6) " " (nth m  7))
+  (log (nth m  8) " " (nth m  9) " " (nth m 10) " " (nth m 11))
+  (log (nth m 12) " " (nth m 13) " " (nth m 14) " " (nth m 15)))
+
+(def g-foo (ref {:x 0 :y 0}))
+
+(defn handle-mouse-pressed [event]
+  (let [e (:data event)
+        #^GLU glu (GLU.)
+        #^GL  gl (GLU/getCurrentGL)
+        viewport (make-array Integer/TYPE 4)
+        modelview (make-array Double/TYPE 16)
+        projection (make-array Double/TYPE 16)
+        world-coords-near (make-array Double/TYPE 4)
+        world-coords-far  (make-array Double/TYPE 4)
+        x (double (.getX e))
+        y (double (.getY e))
+        z (double (- (:z @g-camera)))]
+    (doto gl
+      (.glMatrixMode GL/GL_MODELVIEW)
+      (.glGetIntegerv GL/GL_VIEWPORT          viewport   0)
+      (.glGetDoublev  GL/GL_MODELVIEW_MATRIX  modelview  0)
+      (.glGetDoublev  GL/GL_PROJECTION_MATRIX projection 0))
+    (log "x: " x)
+    (log "y: " (- (nth viewport 3) y))
+    (log "z: " z)
+    (log "modelview:")
+    (log-matrix modelview)
+    (log "projection:")
+    (log-matrix projection)
+    (.gluUnProject glu x (- (nth viewport 3) y) 0
+                   modelview         (int 0)
+                   projection        (int 0)
+                   viewport          (int 0)
+                   world-coords-near (int 0))
+    (.gluUnProject glu x (- (nth viewport 3) y) 1.0
+                   modelview         (int 0)
+                   projection        (int 0)
+                   viewport          (int 0)
+                   world-coords-far  (int 0))
+    (log "near: ")
+    (log (nth world-coords-near 0) " "
+         (nth world-coords-near 1) " "
+         (nth world-coords-near 2) " "
+         (nth world-coords-near 3) " ")
+    (log "far: ")
+    (log (nth world-coords-far 0) " "
+         (nth world-coords-far 1) " "
+         (nth world-coords-far 2) " "
+         (nth world-coords-far 3) " ")
+    (dosync (alter g-foo (fn [_] {:x (* (+ (nth world-coords-near 0)
+                                           (nth world-coords-far 0))
+                                        (/ z 10000.0))
+                                  :y (* (+ (nth world-coords-near 1)
+                                           (nth world-coords-far 1))
+                                        (/ z 10000.0))})))))
+
+(def g-events (ref []))
+
+(defn fire-event [event]
+  (dosync (alter g-events conj event)))
+
+(defn process-event [event]
+;  (log event)
+  (cond (= "mousePressed" (:type event)) (handle-mouse-pressed event)
+        :else (log "Unhandled event: " event)))
+
+(defn process-events []
+  (doseq [event @g-events]
+    (process-event event))
+  (dosync (alter g-events (fn [_] []))))
+
 (defn gl-init [canvas]
-;  (println "gl-init:" canvas)
+;  (log "gl-init:" canvas)
   (let [gl (.getGL canvas)]
     (doto gl
       (.setSwapInterval 1)
@@ -21,7 +104,7 @@
       (.glEnable GL/GL_DEPTH_TEST))))
 
 (defn gl-reshape [canvas x y w h]
-;  (println "gl-reshape:" canvas x y w h)
+;  (log "gl-reshape:" canvas x y w h)
   (let [gl (.getGL canvas)
         a (double (/ w h))]
     (doto gl
@@ -37,10 +120,11 @@
 (def g-camera (ref {:x 0 :y 0 :z -10}))
 
 (defn gl-display [canvas]
-;  (println "gl-display" canvas)
+  (process-events)
+;  (log "gl-display" canvas)
   (let [#^GL gl (.getGL canvas)
         dt (- (get-time) @t)]
-;    (println (double dt))
+;    (log (double dt))
     (dosync (alter t get-time))
     (dosync (alter x + (/ dt 50.0)))
 ;    (when (> @x 1) (dosync (alter x (fn [_] -1))))
@@ -49,12 +133,12 @@
       (.glClear (bit-or GL/GL_COLOR_BUFFER_BIT GL/GL_DEPTH_BUFFER_BIT))
       (.glMatrixMode GL/GL_MODELVIEW)
       (.glLoadIdentity)
+      (.glPushMatrix)
       (.glTranslatef (:x @g-camera)
                      (:y @g-camera)
                      (:z @g-camera))
       (.glPushMatrix)
-      (.glTranslatef -10 0 0)
-      (.glRotatef @x 0 0 1)
+      (.glTranslatef (:x @g-foo) (:y @g-foo) 0)
       (.glBegin GL/GL_POLYGON)
       (.glColor3f   1  0 0)
       (.glVertex3f -5 -5 0)
@@ -66,18 +150,7 @@
       (.glVertex3f -5  5 0)
       (.glEnd)
       (.glPopMatrix)
-      (.glTranslatef 10 0 0)
-      (.glRotatef @x 0 0 1)
-      (.glBegin GL/GL_POLYGON)
-      (.glColor3f   1  0 0)
-      (.glVertex3f -5 -5 0)
-      (.glColor3f   0  1 0)
-      (.glVertex3f  5 -5 0)
-      (.glColor3f   1  1 1)
-      (.glVertex3f  5  5 0)
-      (.glColor3f   0  0 1)
-      (.glVertex3f -5  5 0)
-      (.glEnd))))
+      (.glPopMatrix))))
 
 (defn get-gl-app []
   (proxy [GLEventListener] []
@@ -85,51 +158,50 @@
     (reshape [canvas x y w h] (gl-reshape canvas x y w h))
     (display [canvas] (gl-display canvas))
     (displayChanged [canvas mode-changed device-changed]
-      (println "DisplayChanged:" canvas mode-changed device-changed))))
+      (log "DisplayChanged:" canvas mode-changed device-changed))))
 
 (defn mouse-entered [e]
-;  (println "mouseEntered:" e)
+;  (log "mouseEntered:" e)
   )
 
 (defn mouse-exited [e]
-;  (println "mouseExited:" e)
+;  (log "mouseExited:" e)
   )
 
 (defn mouse-pressed [e]
   (def mx (.getX e))
   (def my (.getY e))
-;  (println "mousePressed:" e)
-  )
+;  (log "mousePressed:" e)
+  (fire-event {:type "mousePressed" :data e}))
 
 (defn mouse-released [e]
-;  (println "mouseReleased:" e)
+;  (log "mouseReleased:" e)
   )
 
 (defn mouse-clicked [e]
-;  (println "mouseClicked:" e)
+;  (log "mouseClicked:" e)
   )
 
 (defn mouse-dragged [e]
-;  (println "mouseDragged:" e)
+;  (log "mouseDragged:" e)
   (let [dx (/ (- (.getX e) mx) 15) 
         dy (/ (- my (.getY e)) 15)
         cx (:x @g-camera)
         cy (:y @g-camera)]
-    (dosync (alter g-camera assoc :x (+ cx dx)
-                                  :y (+ cy dy))))
+;    (dosync (alter g-camera assoc :x (+ cx dx)
+;                                  :y (+ cy dy))))
   (def mx (.getX e))
-  (def my (.getY e)))
+  (def my (.getY e))))
 
 (defn mouse-moved [e]
-;  (println "mouseMoved:" e)
+;  (log "mouseMoved:" e)
   )
 
 (defn mouse-wheel-moved [e]
-;  (println "mouseWheelMoved:" e)
+;  (log "mouseWheelMoved:" e)
   (let [dz (/ (.getWheelRotation e) 2)
         cz (:z @g-camera)]
-    (dosync (alter g-camera assoc :z (+ cz dz)))
-    (println (:z @g-camera))))
+    (dosync (alter g-camera assoc :z (+ cz dz)))))
 
 
 (defn get-mouse-handler []
@@ -165,6 +237,6 @@
         (Thread/sleep 1000)
         (recur)))))
 
-(println "=== begin ===")
+(log "=== begin ===")
 (go)
-(println "=== end ===")
+(log "=== end ===")
